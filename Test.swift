b644ea8e7578f225fafff087db331184d18191e4
maqudah <maqudah@ymail.com>
2017-11-11 11:55:23 +0200

    Add Test

---
+++ Test.swift
@@ -0,0 +1,14 @@
+//
+//  Test.swift
+//  MQIslamicPrayerTimes
+//
+//  Created by Mustafa Alqudah on 11/11/17.
+//
+
+import Foundation
+
+class Test {
+    init() {
+        print("!@#$%")
+    }
+}
