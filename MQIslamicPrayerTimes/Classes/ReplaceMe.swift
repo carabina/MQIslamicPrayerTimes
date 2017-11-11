9330a7e86cfd30632c8402d6a8b0ee430f1862b8
maqudah <maqudah@ymail.com>
2017-11-11 12:15:37 +0200

    sad

---
+++ MQIslamicPrayerTimes/Classes/ReplaceMe.swift
@@ -0,0 +1,15 @@
+//
+//  Test 2.swift
+//  Pods
+//
+//  Created by Mustafa Alqudah on 11/11/17.
+//
+
+import Foundation
+
+public class Test3 {
+    init() {
+        print("Mustafa3")
+    }
+}
+
